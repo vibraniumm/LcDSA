8b3c305305dcbbc501a9ee12e5a219dd0fd22495
vibraniumm <singhluckyy19@gmail.com>
2026-05-12 00:31:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (97.12%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+
+                    backtrack(path)
+
+                    path.pop()
+
+        backtrack([])
+
+        return result
