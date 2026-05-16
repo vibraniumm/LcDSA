16dd6b4699116cdf1df45375e318be232e4e7569
vibraniumm <singhluckyy19@gmail.com>
2026-05-16 20:32:31 +0530

    Time: 8 ms (70.11%), Space: 21.4 MB (80.06%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minPathSum(self, grid):
+        m, n = len(grid), len(grid[0])
+
+        # First row
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+
+        # First column
+        for i in range(1, m):
+            grid[i][0] += grid[i - 1][0]
+
+        # Remaining cells
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[m - 1][n - 1]
+        
