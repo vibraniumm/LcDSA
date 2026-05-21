d0c735f37b0e102c16d8361135f5a5cc80049371
vibraniumm <singhluckyy19@gmail.com>
2026-05-21 21:06:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (42.25%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def generateMatrix(self, n):
+        matrix = [[0] * n for _ in range(n)]
+
+        top, bottom = 0, n - 1
+        left, right = 0, n - 1
+
+        num = 1
+
+        while left <= right and top <= bottom:
+
+            # Left to Right
+            for j in range(left, right + 1):
+                matrix[top][j] = num
+                num += 1
+            top += 1
+
+            # Top to Bottom
+            for i in range(top, bottom + 1):
+                matrix[i][right] = num
+                num += 1
+            right -= 1
+
+            # Right to Left
+            for j in range(right, left - 1, -1):
+                matrix[bottom][j] = num
+                num += 1
+            bottom -= 1
+
+            # Bottom to Top
+            for i in range(bottom, top - 1, -1):
+                matrix[i][left] = num
+                num += 1
+            left += 1
+
+        return matrix
