210ddde3e4cf3ea5dc74fa383138925847d88057
vibraniumm <singhluckyy19@gmail.com>
2026-06-14 21:15:15 +0530

    Time: 63 ms (7.24%), Space: 19.1 MB (64.8%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseBits(self, n):
+
+        result = 0
+
+        for i in range(32):
+
+            bit = n & 1
+
+            result = (result << 1) | bit
+
+            n >>= 1
+
+        return result
+        
