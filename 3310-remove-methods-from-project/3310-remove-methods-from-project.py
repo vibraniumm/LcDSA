db4a97f3556ead212b23a21b6319ab66aea753b3
vibraniumm <singhluckyy19@gmail.com>
2026-08-05 22:58:47 +0530

    Time: 333 ms (55.3%), Space: 145.6 MB (31.82%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Solution:
+    def remainingMethods(self, n, k, invocations):
+
+        graph = defaultdict(list)
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        suspicious = set()
+
+        def dfs(node):
+            if node in suspicious:
+                return
+
+            suspicious.add(node)
+
+            for nei in graph[node]:
+                dfs(nei)
+
+        dfs(k)
+
+        # Check if any outside node calls a suspicious node
+        for u, v in invocations:
+
+            if u not in suspicious and v in suspicious:
+                return list(range(n))
+
+        ans = []
+
+        for i in range(n):
+            if i not in suspicious:
+                ans.append(i)
+
+        return ans
